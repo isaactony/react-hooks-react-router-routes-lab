947eecd2c5cb4c5525a318236aecae0b43968d85
isaac <isaactonyloi@gmail.com>
2023-01-24 10:56:50 +0300

    competed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (   
+  <>
+    <h1>Actors Page</h1>
+    {actors.map(actor => (
+      <div key={actor.name}>
+        <p>Actor: {actor.movies}</p>
+        <ul>
+          {actor.movies.map(movie => (
+            <li key={movie}>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </>)
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <>
+    <h1>Directors Page</h1>
+    {directors.map(director => (
+      <div key={director.name}>
+        <p>Director: {director.name}</p>
+        <ul>
+          {director.movies.map(movie => (
+            <li>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </>)
 }
 
 export default Directors;
